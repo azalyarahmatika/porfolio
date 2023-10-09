841369b0266263b068592e3bc055258f6cf5d5a1
Azalya Rahmatika <azalyarahmatika@gmail.com>
2023-10-09 13:19:25 +0300

    finishing

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  })
-})
+function buttonHandler() {
+  // const frontButtons = document.querySelectorAll('.front_button');
+  const roles = document.querySelectorAll('.role');
+  console.log(roles[0].children[1].children[2]);
 
-const hiddenElement = document.querySelectorAll('.hidden');
-console.log(hiddenElement);
-hiddenElement.forEach((el)=>observer.observe(el));
+  roles.forEach((role) => {
+    role.children[0].children[3].addEventListener('click', () => {
+      if(role.children[0].classList.contains('back_transform')) {
+        role.children[0].classList.remove('back_transform');
+      }
+      role.children[0].classList.add('transform');
+      role.children[1].classList.add('visible');
+    })
+
+    role.children[1].children[1].addEventListener('click', () => {
+      if(role.children[0].classList.contains('transform')) {
+        role.children[0].classList.remove('transform');
+      }
+      role.children[0].classList.add('back_transform');
+      role.children[1].classList.remove('visible');
+      // role.children[0].classList.remove('transform');
+      // console.log(frontCard.children[3]);
+    })
+  })
+};
+
+document.addEventListener("DOMContentLoaded", buttonHandler);
